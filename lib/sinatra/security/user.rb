59fa4b99674f76afd976fd4a13fee97a3f9a8c83
Cyril David <cyx.ucron@gmail.com>
2010-05-26 05:50:48 +0800

    added documentation for Sinatra::Security::User

--- lib/sinatra/security/user.rb
+++ lib/sinatra/security/user.rb
@@ -1,5 +1,30 @@
 module Sinatra
   module Security
+    # Mixes in to any included class all of the following:
+    # 
+    # * Sinatra::Security::LoginField
+    # * Sinatra::Security::Password
+    # * Sinatra::Security::Validations
+    #
+    # It also extends the class with Sinatra::Security::Identification.
+    #
+    # @example
+    #   
+    #   class User < Ohm::Model
+    #     include Sinatra::Security::User
+    #   end
+    #
+    #   user = User.new
+    #   user.valid?
+    #   user.errors == [[:email, :not_present], [:password, :not_present]]
+    #   # => true
+    #
+    #   user = User.create(:email => "test@example.com", :password => "pass",
+    #                      :password_confirmation => "pass")
+    #
+    #   User.authenticate("test@example.com", "pass") == user
+    #   # => true
+    #
     module User
       def self.included(user)
         user.send :include, LoginField
